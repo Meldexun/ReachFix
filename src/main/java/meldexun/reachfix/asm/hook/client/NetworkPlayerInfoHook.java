a9eafb3dfa7c3ee833c49ca8ebbc3f48671db946
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-01-28 17:17:53 +0100

    Fix updating reach modifier of wrong player

--- src/main/java/meldexun/reachfix/asm/hook/client/NetworkPlayerInfoHook.java
+++ src/main/java/meldexun/reachfix/asm/hook/client/NetworkPlayerInfoHook.java
@@ -3,12 +3,21 @@ package meldexun.reachfix.asm.hook.client;
 import meldexun.reachfix.util.ReachAttributeUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.network.NetworkPlayerInfo;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.GameType;
 
 public class NetworkPlayerInfoHook {
 
 	public static void onUpdateGameMode(NetworkPlayerInfo playerInfo, GameType newGameMode) {
-		ReachAttributeUtil.onGameModeChanged(Minecraft.getMinecraft().player, playerInfo.getGameType(), newGameMode);
+		Minecraft mc = Minecraft.getMinecraft();
+		if (mc.world == null) {
+			return;
+		}
+		EntityPlayer player = mc.world.getPlayerEntityByUUID(playerInfo.getGameProfile().getId());
+		if (player == null) {
+			return;
+		}
+		ReachAttributeUtil.onGameModeChanged(player, playerInfo.getGameType(), newGameMode);
 	}
 
 }
