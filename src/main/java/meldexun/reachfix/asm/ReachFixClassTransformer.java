5ac66eefd21e24b080d90b70badf93892ac835d4
Meldexun <27779321+Meldexun@users.noreply.github.com>
2025-03-21 02:22:06 +0100

    Fix deobfuscation map loading

--- src/main/java/meldexun/reachfix/asm/ReachFixClassTransformer.java
+++ src/main/java/meldexun/reachfix/asm/ReachFixClassTransformer.java
@@ -21,17 +21,18 @@ import meldexun.asmutil2.NonLoadingClassWriter;
 import meldexun.asmutil2.reader.ClassUtil;
 import net.minecraft.launchwrapper.IClassTransformer;
 import net.minecraft.launchwrapper.Launch;
-import net.minecraftforge.fml.common.asm.transformers.deobf.FMLDeobfuscatingRemapper;
 
 public class ReachFixClassTransformer extends HashMapClassNodeClassTransformer implements IClassTransformer {
 
 	private static final ClassUtil REMAPPING_CLASS_UTIL;
 	static {
 		try {
-			Field _classNameBiMap = FMLDeobfuscatingRemapper.class.getDeclaredField("classNameBiMap");
+			Class<?> FMLDeobfuscatingRemapper = Class.forName("net.minecraftforge.fml.common.asm.transformers.deobf.FMLDeobfuscatingRemapper", true, Launch.classLoader);
+			Field _INSTANCE = FMLDeobfuscatingRemapper.getField("INSTANCE");
+			Field _classNameBiMap = FMLDeobfuscatingRemapper.getDeclaredField("classNameBiMap");
 			_classNameBiMap.setAccessible(true);
 			@SuppressWarnings("unchecked")
-			BiMap<String, String> deobfuscationMap = (BiMap<String, String>) _classNameBiMap.get(FMLDeobfuscatingRemapper.INSTANCE);
+			BiMap<String, String> deobfuscationMap = (BiMap<String, String>) _classNameBiMap.get(_INSTANCE.get(null));
 			REMAPPING_CLASS_UTIL = ClassUtil.getInstance(new ClassUtil.Configuration(Launch.classLoader, deobfuscationMap.inverse(), deobfuscationMap));
 		} catch (ReflectiveOperationException e) {
 			throw new UnsupportedOperationException(e);
